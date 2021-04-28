a12575b994993841bfedff8a7a1aee6a91f9cad0
kailous <kailous@live.cn>
2021-04-28 10:09:54 +0800

    未完成

---
+++ js/Weather.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#main',
+    data: {
+        navs:[
+            { text: 'WEATHER' },
+            { text: 'discord' },
+            { text: 'Greeting' },
+            { text: 'music' },
+            { text: 'time' }
+        ]
+    }
+})
---
+++ js/nav.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#nav',
+    data: {
+        navs: [
+            {
+                text: 'WEATHER',
+                url: 'https://baidu.com'
+            },
+            {
+                text: 'discord',
+                url: 'https://google.com'
+            },
+            {
+                text: 'Greeting',
+                url: 'https://baidu.com'
+            },
+            {
+                text: 'music',
+                url: 'https://baidu.com'
+            },
+            {
+                text: 'time',
+                url: 'https://baidu.com'
+            }
+        ]
+    }
+})
